649c3eca8f101a8f91cd8487b440b7b4a9fef0d3
Sergio Guilherme Castilho <65431005+SGCastilho@users.noreply.github.com>
2023-11-14 14:34:02 -0300

    Soundtrack controller started

---
+++ source-code-zombie-survival/Assets/_Scripts/Controller/SoundtrackController.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Core.Controllers
+{
+    public sealed class SoundtrackController : Singleton<SoundtrackController>
+    {
+        private AudioSource _audioSource;
+
+        [Header("Settings")]
+        [SerializeField] [Range(0.2f, 1f)] private float _maxVolume = 0.6f;
+        [SerializeField] [Range(0.2f, 1f)] private float _fadeDuration = 0.6f;
+
+        private bool _executingFade;
+
+        public override void Awake() 
+        {
+            if(_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = GetComponent<SoundtrackController>();
+            _audioSource = GetComponent<AudioSource>();
+
+            DontDestroyOnLoad(gameObject);
+
+            if(_instance == null)
+                return;
+        }
+
+        public void PlaySoundtrack(AudioClip audioClip)
+        {
+            if(_executingFade) return;
+
+            StartCoroutine(ChangeSoundtrack(audioClip));
+        }
+
+        public void FadeIn()
+        {
+            if(_executingFade) return;
+
+            StartCoroutine(Fade(true));
+        }
+
+        public void FadeOut()
+        {
+            if(_executingFade) return;
+
+            StartCoroutine(Fade(false));
+        }
+
+        private IEnumerator Fade(bool fadeIn)
+        {
+            _executingFade = true;
+
+            float time = 0f;
+            float startVol = _audioSource.volume;
+            float targetVolume = fadeIn ? _maxVolume : 0f;
+
+            while(time < _fadeDuration)
+            {
+                time += Time.deltaTime;
+                _audioSource.volume = Mathf.Lerp(startVol, targetVolume, time / _fadeDuration);
+
+                yield return null;
+            }
+
+            _executingFade = false;
+
+            yield break;
+        }
+
+        private IEnumerator ChangeSoundtrack(AudioClip clipToPlay)
+        {
+            _executingFade = true;
+
+            float time = 0f;
+            float startVol = _audioSource.volume;
+            float targetVolume = 0f;
+
+            while(time < _fadeDuration)
+            {
+                time += Time.deltaTime;
+                _audioSource.volume = Mathf.Lerp(startVol, targetVolume, time / _fadeDuration);
+
+                yield return null;
+            }
+
+            _audioSource.Stop();
+            _audioSource.clip = clipToPlay;
+            _audioSource.volume = _maxVolume;
+
+            yield return new WaitForSecondsRealtime(_fadeDuration);
+
+            _audioSource.Play();
+
+            _executingFade = false;
+
+            yield break;
+        }
+    }
+}
